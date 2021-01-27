82ae4940f50b44ae9ac5850bae744004ae54183f
Sebastian Bila <sebastianbila.sw@gmail.com>
2021-01-27 21:40:07 +0200

    Update exporting data

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-const responses = require('./responses')
+const { responses } = require('./responses')
+const responseAndStatuses = require('./responses')
 const errors = require('./errors')
 const middlewares = require('./middlewares')
 const consts = require('./consts')
 
 module.exports = {
   ...responses,
+  responseAndStatuses,
   errors,
   middlewares,
   consts
