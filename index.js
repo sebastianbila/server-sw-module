f71eec9e72f25470a3e612ded66147127a7f9514
Sebastian Bila <sebastianbila.sw@gmail.com>
2021-01-27 21:24:00 +0200

    Update middlewares and exporting data

--- index.js
+++ index.js
@@ -1,14 +1,5 @@
-const { HTTP_CODE, CONTENT_TYPE } = require('./lib/consts')
-const { BaseError, InternalServerError, ValidationError } = require('./lib/errors')
-const baseErrorMiddleware = require('./lib/middlewares/baseErrorMiddleware')
 const responses = require('./lib/responses')
 
 module.exports = {
-  HTTP_CODE,
-  CONTENT_TYPE,
-  BaseError,
-  InternalServerError,
-  ValidationError,
-  baseErrorMiddleware,
   ...responses
 }
